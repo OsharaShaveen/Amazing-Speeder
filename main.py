c6b25a23021bbe37321515a835a9d607fdaf8075
White Devil <94506157+OsharaShaveen@users.noreply.github.com>
2022-01-19 14:34:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import asyncio
 from pytgcalls import idle
 from driver.veez import call_py, bot
 
-async def mulai_bot():
-    print("[VEEZ]: STARTING BOT CLIENT")
+async def start_bot():
     await bot.start()
-    print("[VEEZ]: STARTING PYTGCALLS CLIENT")
+    print("[INFO]: BOT & UBOT CLIENT STARTED !!")
     await call_py.start()
+    print("[INFO]: PYTGCALLS CLIENT STARTED !!")
     await idle()
-    await pidle()
-    print("[VEEZ]: STOPPING BOT & USERBOT")
+    print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(mulai_bot())
+loop.run_until_complete(start_bot())
